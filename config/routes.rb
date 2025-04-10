a126b59b03e61fc6677d7d1b746b4141e152e7ec
Guilherme Giachellin <guilherme.giache@gmail.com>
2025-04-10 01:03:44 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "search", to: "searches#index", as: :search
   get "search/results", to: "searches#results", as: :search_results
+  get "search/suggestions", to: "searches#suggestions", as: :search_suggestions
 
   root "searches#index"
 end
