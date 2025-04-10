e119e8f95d2156d544d47907a6f127ce4b6aeeaa
Guilherme Giachellin <guilherme.giache@gmail.com>
2025-04-10 01:04:12 -0300

    Created search suggestions logic

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,13 +3,36 @@ class SearchesController < ApplicationController
     
   end
 
-  def results
-    @query = params[:query]
+  def suggestions
+    term = params[:term]
+  
+    suggestions = if term.present?
+      Search.where("search_text ILIKE ?", "%#{term}%")
+            .select(:search_text)
+            .distinct
+            .limit(5)
+            .pluck(:search_text)
+    else
+      []
+    end
+  
+    render json: suggestions
+  end
 
+  def results
+    @query = params[:query]&.strip
+  
     if @query.present?      
+      Search.where("LENGTH(search_text) < LENGTH(?)", @query)
+            .where("? ILIKE search_text || '%'", @query)
+            .delete_all  
+      
+      Search.find_or_create_by(search_text: @query)
+  
       @results = Article.where("title ILIKE :q", q: "%#{@query}%")
     else
       @results = []
     end
   end
+  
 end
